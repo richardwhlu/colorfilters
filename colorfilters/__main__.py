b89d782509e5b9c3c278ea2dc95cc3aaaf6bb7ca
Richard Lu <richard.wh.lu@gmail.com>
2021-12-25 15:00:06 -0800

    Update __main__.py

--- colorfilters/__main__.py
+++ colorfilters/__main__.py
@@ -21,9 +21,16 @@ if __name__ == "__main__":
     parser.add_argument(
         "colorspace", choices=list(choices.keys()), help="colorspace to filter in"
     )
+    parser.add_argument(
+        "resize_factor_w", help="factor to resize width"
+    )
+    parser.add_argument(
+        "resize_factor_h", help="factor to resize height"
+    )
     args = parser.parse_args()
 
     img = cv.imread(args.image)
+    img = cv.resize(img, (int(img.shape[0] / args.resize_factor_w), int(img.shape[1] / args.resize_factor_h)))
     if img is None or img.size == 0:
         raise Exception(f"Unable to read image {args.image}. Please check the path.")
     window = choices[args.colorspace](img)
